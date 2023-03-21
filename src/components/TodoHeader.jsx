11f596031396e8c6da6fc61f7da79f89ae977586
hcs1105 <hcs1105@gmail.com>
2023-03-21 21:01:04 +0900

    헤더 영역 스타일 적용

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './TodoHeader.module.css'
 
-export default function TodoHeader({filters, onFilterChange}) {
+export default function TodoHeader({filters, filter, onFilterChange}) {
   return (
-    <header>
-      <div>
-        {filters.map((value, index) => <button type="button" key={index} onClick={() => onFilterChange(value)}>{value}</button>)}
+    <header className={styles.header}>
+      <div className={styles.button_area}>
+        {filters.map((value, index) => <button type="button" className={`${styles.button_filter} ${filter === value && styles.active}`} key={index} onClick={() => onFilterChange(value)}>{value}</button>)}
       </div>
     </header>
   );
