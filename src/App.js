2b01ccd4c0e27f87ea944dc3a50e5f82766d88cf
hcs1105 <hcs1105@gmail.com>
2023-04-14 19:51:17 +0900

    useContext()를 다크모드일 때와 아닐 때의 기본 로직 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import TodoHeader from './components/TodoHeader';
 import TodoList from './components/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 function App() {
@@ -9,10 +10,10 @@ function App() {
   const onFilterChange = filter => setFilter(filter);
   
   return (
-    <>
+   <DarkModeProvider>
       <TodoHeader filters={filters} filter={filter} onFilterChange={onFilterChange} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from './TodoHeader.module.css'
+import { useDarkMode } from '../context/DarkModeContext';
+import { HiMoon, HiSun } from 'react-icons/hi'
 
 export default function TodoHeader({filters, filter, onFilterChange}) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+  const handleDarkMode = () => toggleDarkMode();
+
   return (
     <header className={styles.header}>
+      <button type="button" onClick={handleDarkMode}>
+        {!darkMode ? <HiMoon className={styles.icons} /> : <HiSun className={styles.icons} />}
+      </button>
       <div className={styles.button_area}>
         {filters.map((value, index) => <button type="button" className={`${styles.button_filter} ${filter === value && styles.active}`} key={index} onClick={() => onFilterChange(value)}>{value}</button>)}
       </div>
