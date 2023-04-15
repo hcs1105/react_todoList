809d349124659975a795f0e012d7b3af774df338
hcs1105 <hcs1105@gmail.com>
2023-04-15 15:50:53 +0900

    로컬 스토리지를 이용하여 Todo Item 저장하기

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddToDoForm from './AddToDoForm';
 import TodoItem from './TodoItem';
 import styles from './TodoList.module.css'
 
 export default function TodoList({filter}) {
+  const [todoList, setTodoList] = useState(() => readTodoListFromLocalStorage());
+  // const [todoList, setTodoList] = useState(localStorage.getItem('todoList'));
+
+  /*
   const [todoList, setTodoList] = useState([
     {id:1, content:'공부하기', status:'active'},
     {id:2, content:'바둑 두기', status:'active'},
   ]);
-
+  */
   const handleAdd = todo => {
     // 새로운 todo를 todeList에 업데이트
     console.log(todo);
@@ -26,12 +30,23 @@ export default function TodoList({filter}) {
   };
 
   const filtered = getFilteredItem(todoList, filter);
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList)); // 객체를 배열로 저장하기 위해 JSON으로 변환
+  }, [todoList]);
   
   function getFilteredItem(todoList, filter) {
     if(filter === 'all') {
       return todoList;
     }
     return todoList.filter(todo => todo.status === filter);
+  }
+
+  function readTodoListFromLocalStorage() {
+    console.log(readTodoListFromLocalStorage);
+    const todoList = localStorage.getItem('todoList');
+
+    return todoList ? JSON.parse(todoList) : [];
   }
 
   return (
